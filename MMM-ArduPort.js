5047fccdd27b0818885587c68f5ecbf79476f4bf
Jose Padron <elrafapadron@gmail.com>
2024-04-20 18:51:30 -0400

    Update MMM-ArduPort.js

--- MMM-ArduPort.js
+++ MMM-ArduPort.js
@@ -5,6 +5,14 @@
 // file LICENSE, which is part of this source code package, for details.
 // ====================================================
 
+String.prototype.format = function() {
+   var formatted = this;
+   for( var arg in arguments ) {
+       formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+   }
+   return formatted;
+};
+
 Module.register("MMM-ArduPort", {
 
     logPrefix: '[MMM-ArduPort]:: ',
